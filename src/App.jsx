e2bbaa16c4af53cedb0b2937cdd64e9959a4a749
Patrik <patriknil90@gmail.com>
2019-09-13 23:25:42 +0200

    Add alias imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import HelloWorld from 'components/HelloWorld'
 
-const App = () => <h1>Minimal React Starter Kit</h1>
+const App = () => (
+  <>
+    <h1>Minimal React Starter Kit</h1>
+    <HelloWorld />
+  </>
+)
 
 export default App
