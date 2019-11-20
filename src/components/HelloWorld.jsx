d49a9a2719b2991cd7c195808c5345f4ab164575
Patrik <patriknil90@gmail.com>
2019-11-20 19:56:19 +0100

    Use styled component in HelloWorld.jsx

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h2`
+  color: tomato;
+`
 
 class HelloWorld extends React.Component {
   render() {
-    return <h2>Hello World!</h2>
+    return <Title>Hello World!</Title>
   }
 }
 
